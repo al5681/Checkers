4f877c6f31b14c2588169db577b03f236a18e167
al5681 <al5681@outlook.com>
2021-06-04 18:55:01 +0100

    added test class and constructor for CheckersBoard

--- src/CheckersBoard.java
+++ src/CheckersBoard.java
@@ -3,4 +3,22 @@ public class CheckersBoard {
     private Player playerBlack;
     private Player playerwhite;
     private Tile[][] board;
+
+    // creates an 8x8 board, every other tile is usable (dark brown)
+    public CheckersBoard()
+    {
+        for(int row = 1; row < board.length+1; row++)
+        {
+            for(int col = 1; col < board[row].length+1; col++)
+            {
+                if(row % 2 == 0)
+                {
+                    board[row][col] = new Tile(false);
+                }
+                else {
+                    board[row][col] = new Tile(true);
+                }
+            }
+        }
+    }
 }
--- src/Tile.java
+++ src/Tile.java
@@ -6,4 +6,6 @@ public class Tile {
     {
         this.usable = usable;
     }
+
+
 }
