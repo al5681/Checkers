74143d82a04161f7e3ff28310b3228e19980bf09
al5681 <al5681@outlook.com>
2021-06-05 13:24:23 +0100

    rendered board in GUI

--- src/CheckersGUI.java
+++ src/CheckersGUI.java
@@ -1,12 +1,35 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class CheckersGUI extends Application {
+
+    private Checkers checkers = new Checkers();
+    private GridPane grid = new GridPane();
+
+
+    public GridPane renderBoard() {
+        for (int i = 0; i < checkers.getCheckersBoard().getRows(); i++) {
+            for (int j = 0; j < checkers.getCheckersBoard().getCols(); j++) {
+                Tile currTile = checkers.getCheckersBoard().getBoard()[i][j];
+                Button currButton = new Button();
+                if (currTile.isDarkBrown()) {
+                    currButton.setStyle("-fx-background-color:#96652c; -fx-background-radius: 0");
+                } else {
+                    currButton.setStyle("-fx-background-color:#e6c9aa; -fx-background-radius: 0");
+                }
+                currButton.setPrefHeight(100);
+                currButton.setPrefWidth(100);
+                GridPane.setRowIndex(currButton, i);
+                GridPane.setColumnIndex(currButton, j);
+                grid.getChildren().add(currButton);
+            }
+        }
+        return grid;
+    }
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -14,8 +37,8 @@ public class CheckersGUI extends Application {
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Checkers");
-        StackPane root = new StackPane();
-        primaryStage.setScene(new Scene(root, 300, 250));
+        renderBoard();
+        primaryStage.setScene(new Scene(grid));
         primaryStage.show();
     }
 }
